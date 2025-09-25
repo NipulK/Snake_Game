19803a1e6167eec6cb92840cc8684ce811c37c63
NipulK <168514285+NipulK@users.noreply.github.com>
2025-09-25 15:42:19 +0530

    Start the project and set up the game screen

---
+++ snakegame.py
@@ -0,0 +1,13 @@
+import turtle
+import time
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+
+wn.tracer(0)  # Turns off the screen updates    
+
+
+wn.mainloop()
