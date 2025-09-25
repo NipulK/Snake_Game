8d60e7ad6a57503b5a0a553e6015cc3c1fe82233
NipulK <168514285+NipulK@users.noreply.github.com>
2025-09-25 16:21:12 +0530

    update the snake directions

--- snakegame.py
+++ snakegame.py
@@ -17,7 +17,7 @@ head.speed(0)
 head.shape("square")
 head.color("white")
 head.penup()
-head.goto(0, 0)
+head.goto(0, 20)
 head.direction = "up"
 
 # Functions to control the snake
@@ -25,6 +25,15 @@ def move():
     if head.direction == "up":
         y = head.ycor()
         head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
 
 
 # Main game loop
